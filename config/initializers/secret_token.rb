dfe4e8de986c86c34a03c6b4e5381ed4cec2d9ef
Quinn Rohlf <qr@qrohlf.com>
2013-11-02 17:23:19 -0700

    vanilla Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rockthang::Application.config.secret_key_base = '3b9f2aa5b9b7066569aed6b6559510f22a1879cbd77374b8f4cda09986f8823bc205aa3cca087cf791df6ebd4be06d64f1f233dd6616294b8acf383ff76dab1c'
