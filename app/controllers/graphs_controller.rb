3f5e32aeb2b6ca4df04e78876d699c8bf770336d
Quinn Rohlf <qr@qrohlf.com>
2013-11-04 17:51:56 -0800

    remove unneeded sigmajs code from graph controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,14 +2,7 @@ class GraphsController < ApplicationController
   def show
     # Show a graph of all the climbs in the database
     # hideously hacky solution that should be done client-side.
-    @addnodejs = ""
-    Climb.all.each do |climb| 
-        @addnodejs += "nodes.push({'id': #{climb.id}', 'name': label: '#{climb.name}');\n"
-    end
-    @edgesjs = ""
-    Path.all.each do |path|
-        @edgesjs += "sigInst.addEdge('#{path.id}', '#{path.climb1.id}', '#{path.climb2.id}');\n"
-    end
+    
   end
 
   def json
